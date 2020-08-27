bf7bf9c87517568402ccdbf1b6713243e4c45291
meloegel <meloegel@gmail.com>
2020-08-27 16:54:51 -0400

    fixed

--- src/components/cell.js
+++ src/components/cell.js
@@ -5,7 +5,7 @@ var numberToWords = require('number-to-words')
 const Cell = ({ cell, flip, i, j }) => {
     let life = 'life'
     return (
-        <div className={`cell ${cell.currentStatus ? life : 'dead'}`} id={numberToWords.toWords(cell.lifeCount)} onClick={() => flip(i, j)} />
+        <div id={numberToWords.toWords(cell.lifeCount)} className={`cell ${cell.currentStatus ? life : 'dead'}`} onClick={() => flip(i, j)} />
     )
 }
 export default Cell
